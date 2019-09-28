5319af151de8015ca65a238b7b5ad29ad496f913
BiscutTriscut <davidearlbowlesjr@gmail.com>
2019-09-28 17:49:27 -0400

    Person Subclasses

---
+++ Housing.java
@@ -0,0 +1,18 @@
+
+package javaapplication25;
+
+/**
+ *
+ * @author biscu
+ */
+public class Housing extends Person{
+
+boolean rentTrouble; //If the person needs help paying rent or mortgage
+
+boolean subsidizedHousing; //If person needs subsidized housing
+
+boolean voucher; //If person needs vouchers for private housing
+
+int housingType; //What kind of house are they living in? 
+
+}
