514a547e8a3631b9f3ff512c116c77b50423f702
Mike Calvo <calvo.michael1@gmail.com>
2019-09-28 22:34:07 -0400

    Updated Person, logic controller, and General class

--- PersonController.java
+++ PersonController.java
@@ -1,12 +1,32 @@
 public class PersonController {
+    //Housing Variables
+    String rentTrouble;
+    String subsidizedHousing;
+    String voucher;
+    String housingType;
 
-    Housing housing = new Housing();
-    MentalHealth mhealth = new MentalHealth();
-    MilitaryBranch branch = new MilitaryBranch();
-    PhysicalHealth phealth = new PhysicalHealth();
-    SubstanceAbuse subAbuse = new SubstanceAbuse();
+    //Mental Health Variables
+    String mentalHealthString;
 
-    Person person = new Person(mhealth, housing, branch, phealth, subAbuse);
+    //Military Branch Variables
+    String branch;
+    String fullTime;
+    String serviceLength;
+
+    //Physical Health String
+    String physicalHealthString;
+
+    //Substance Abuse String
+    String subAbuseString;
+
+
+    Housing housing = new Housing(rentTrouble,subsidizedHousing,voucher,housingType);
+    MentalHealth mhealth = new MentalHealth(mentalHealthString);
+    MilitaryBranch Milbranch = new MilitaryBranch(branch,fullTime,serviceLength);
+    PhysicalHealth phealth = new PhysicalHealth(physicalHealthString);
+    SubstanceAbuse subAbuse = new SubstanceAbuse(subAbuseString);
+
+    Person person = new Person(mhealth, housing, Milbranch, phealth, subAbuse);
 
     //grabing values from the GUI
 
