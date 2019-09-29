00e06fd58340b8c0c08e9c0e1c4eefd3509db807
Mike Calvo <calvo.michael1@gmail.com>
2019-09-29 08:39:38 -0400

    adding database pull

--- DatabaseNG.java
+++ DatabaseNG.java
@@ -3,7 +3,6 @@
  * 
  * 
  */
-package javaapplication25;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -32,16 +31,16 @@ public class DatabaseNG {
 //libs for MilitaryBranch File
     private static final String MILITARYBRANCH_SHEET_ONE="C:\\Users\\biscu\\Desktop\\CodeLinc\\JavaApplication25\\topics\\MilitaryBranch";
     
-    String document;
-    private Scanner x;
-    ArrayList<String> arrString = new ArrayList<String>();
+    private static String document;
+    private static Scanner x;
+    private static ArrayList<String> arrString = new ArrayList<String>();
     /**
      * 
      * @param arr boolean values of questions about housing
      * @return depending on what index of arr is true it will output 4 different csvs as an array string
      */
     //Takes a boolean array that has a size of 4
-    public ArrayList getHousing(boolean[] arr) {
+    public static ArrayList getHousing(boolean[] arr) {
         
         //opens and sends the housing type csv as a ArrayList
         if (arr[0] == true) {
@@ -121,7 +120,7 @@ public class DatabaseNG {
  * @param b true if veteran has a form of mental health
  * @return if true returns mentalHealth csv as an ArrayList
  */
-    public ArrayList getMentalHealth(boolean b) {
+    public static ArrayList getMentalHealth(boolean b) {
         //if true opens and sends the mHealth csv as an Array List
         if(b==true){            
         try {
@@ -148,7 +147,7 @@ public class DatabaseNG {
      * @return returns Specific csv files as arrayStrings
      */
 
-    public ArrayList getSubstanceAbuse(boolean b) {
+    public static ArrayList getSubstanceAbuse(boolean b) {
         //if true opens and sends the substanceAbuse csv as an Array List
         if(b == true)  {  
         try {
@@ -175,7 +174,7 @@ public class DatabaseNG {
      * @param b if veteran has a physical health problem
      * @return if b is true returns the pDisability csv as an Array List
      */
-    public ArrayList getPhysicalHealth(boolean b) {
+    public static ArrayList getPhysicalHealth(boolean b) {
         if(b == true) {   
         try{
                 x = new Scanner(new File(PHEALTH_SHEET_ONE));
@@ -200,7 +199,7 @@ public class DatabaseNG {
  * @param arr takes boolean values of military branch, fulltime or part, Service length (5yrs)
  * @return returns Specific csv files as arrayStrings 
  */
-    public ArrayList getMilitaryBranch(boolean[] arr) {
+    public static ArrayList getMilitaryBranch(boolean[] arr) {
         if(arr[0]==true)  {  
         try {
             x = new Scanner(new File(MILITARYBRANCH_SHEET_ONE));
